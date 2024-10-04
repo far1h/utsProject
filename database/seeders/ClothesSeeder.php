a1c728ec93e2599aff3dea1313cfb84e9bfef107
far1h <farihmuhammad45@gmail.com>
2024-10-04 08:45:38 +0700

    Seeding up the tables

--- database/seeders/ClothesSeeder.php
+++ database/seeders/ClothesSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,5 +14,28 @@ class ClothesSeeder extends Seeder
     public function run(): void
     {
         //
+        // 3 methods:
+        // 1. DB::raw
+        // 2. DB::table
+        // 3. Model::
+
+        // 1. -> DB RAW -> Manual Query
+        // DB::raw("INSERT INTO USER BLABLABLA)
+        // DB::raw("SELECT * FROM USERS)
+
+        // 2. -> Query Builder -> Built in laravel
+        // DB::table("users)->insert(['name'=> blabal])
+        // DB::table('users')->get();
+
+        // 3. -> Eloquent - ORM (Object Relational mapping)
+        // User::Insert([]);
+        // User:all();
+
+        Clothes::insert([
+            'name' => 'Uniqlo Airism Oversized',
+            'price'=> '200000',
+            'created_at'=> Carbon::now(),
+            'updated_at'=> Carbon::now()
+            ]);
     }
 }
